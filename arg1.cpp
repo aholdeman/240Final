223a8e34a7720de20a69cc1fec8424368b4c62df
Abby Holdeman <abbyholdeman@Abbys-MacBook-Air.local>
2016-11-28 13:27:17 -0500

    Add existing file

--- arg1.cpp
+++ arg1.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
 	string Input = " ";
 	string sequences[1000];
 	int sequenceIndex = 0;
-
+	
 	while(!ifs.eof()) { //while the scanner is not at the end of the file
 		getline(ifs, Input); //skips the first line that will be an ID
 		getline (ifs, Input); //reads line and assigns it to the string Input
@@ -34,6 +34,7 @@ int main(int argc, char** argv) {
 
 	ifs.close(); //closes input file to prevent memory leak
 
+	//prints the array to see if it works
 	int newSequenceIndex = 0;
 	while(newSequenceIndex <= sequenceIndex) {
 		cout << "Line " << newSequenceIndex+1 << "- " <<  sequences[newSequenceIndex] << endl;
