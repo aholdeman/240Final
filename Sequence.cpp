5ca68078088e2ba7931f219538e22dc899dd8eb5
phoebeanh <phoebeanh@ymail.com>
2016-12-03 17:38:00 -0500

    Add files via upload
    created sequence object

---
+++ Sequence.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Sequence.cpp
+ * Author: PhoebeNgo
+ * 
+ * Created on December 2, 2016, 1:30 PM
+ */
+
+#include "Sequence.h"
+
+Sequence::Sequence() {
+}
+
+Sequence::Sequence(const Sequence& orig) {
+}
+
+Sequence::~Sequence() {
+}
+
+void Sequence::setData(int index, char value) {
+    bool check(true);
+    
+}
+
