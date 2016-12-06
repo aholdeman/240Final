aa3c370df738a42091dca98decd4fc67459051d8
phoebeanh <phoebeanh@ymail.com>
2016-12-05 22:50:43 -0500

    Update Gene.h
    combined Gene.h and Gene2.h

--- Gene.h
+++ Gene.h
@@ -24,13 +24,47 @@ public:
     Gene(ifstream & infile);
     Gene(const Gene& orig);
     
+    Tree();
+    Tree(const Tree& orig);
+    
     void insertRoot(Sequence seq); 
     void compare(Sequence &second);
+    bool empty(); //checks if tree is empty through root
     
     virtual ~Gene();
+    virtual ~Tree();
 private:
     Sequence *sequenceArray;
     int length;
+};
+
+private:
+    Sequence sequenceArray[];
+    Sequence *data;
+    int length;
+    
+    void clear(Node *leaf); //memory leaks
+    void insertLeft(String value, Node *leaf);
+    void insertRight(String value, Node *leaf);
+    Node *search(String value, Node *leaf);
+    Node *root; 
+    void print(Node *root);
+};
+
+
+class Tree {
+public:   
+    void insert(String value); //not sure if need this one
+   // Node *search(String value); //from Node class; //not sure if need this one
+    
+    bool empty(); //checks if tree is empty through root
+    //bool delete(String value); //shouldn't need this one
+private:
+    void clear(Node *leaf); //memory leaks
+    void insert(String value, Node *leaf);
+    Node *search(String value, Node *leaf);
+    Node *root; 
+    void print(Node *root);
 };
 
 #endif /* GENE_H */
