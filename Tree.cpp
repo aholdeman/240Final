dfa4f9c9dc968781b5450581e14f1e17e399c4ee
lcarrnix <lcarrnix@gmail.com>
2016-12-05 21:13:37 -0500

    Add files via upload

--- Tree.cpp
+++ Tree.cpp
@@ -21,13 +21,13 @@ struct Node //structure class for node
     
     Node()
     {
-        left=NULL; //fix this
-        right=NULL; //fix this
+        left='/n'; //fix this to null
+        right='/n'; //fix this to null
     }
 };
 
 Tree::Tree() {
-    root = NULL; //fix this
+    root = '/n'; //fix this to null
 }
 
 Tree::Tree(const Tree& orig) {
@@ -40,7 +40,7 @@ Tree::~Tree() {
 
 void Tree:: clear(Node *leaf)
 {
-    if(leaf != NULL) //fix this
+    if(leaf != '/n') //fix this to null
     {
         clear(leaf->left); //clears left tree
         clear(leaf->right); //clears right tree
@@ -50,19 +50,19 @@ void Tree:: clear(Node *leaf)
 
 bool Tree:: empty()
 {
-    return(root == NULL); //fix this
+    return(root == '/n'); //fix this to null
 }
 
 
-//public search method- user just needs value to search for, not root
-Node Tree:: *search(String value)
-{
-    return search(value, root); //calls actual search method
-}
+////public search method- user just needs value to search for, not root
+//Node Tree:: *search(String value)
+//{
+//    return search(value, root); //calls actual search method
+//}
 
 Node Tree:: *search(String value, Node *leaf)
 {
-    if(leaf!= NULL) //fix this
+    if(leaf!= '/n') //fix this
     {
         if(value==leaf-> key_value) //found 
         {
@@ -79,14 +79,14 @@ Node Tree:: *search(String value, Node *leaf)
     }
     else
     {
-        return NULL; //fix this
+        return '/n'; //fix this to null
     }
 }
 
 //insert function for if root isn't initialized yet
 void Tree:: insert(String value) 
 {
-    if(root != NULL) //call other insert method
+    if(root != '/n') //call other insert method - fix to null
     {
         insert(value, root);
     }
@@ -95,8 +95,8 @@ void Tree:: insert(String value)
         Node *newNode; //create new Node class node in tree
         root = newNode; 
         root -> key_value = value; //sets all equal
-        root -> left = NULL; //establishing tree
-        root -> right = NULL; //establishing tree
+        root -> left = '/n'; //establishing tree // fix to null
+        root -> right = '/n'; //establishing tree // fix to null
     }
 }
 
@@ -104,7 +104,7 @@ void Tree:: insert(String value, Node *leaf)
 {
     if(value < leaf -> key_value) //goes to left tree
     {
-        if(leaf -> left != NULL)
+        if(leaf -> left != '/n') //fix to null
         {
         insert(value, leaf -> left); //recursive call
         }
@@ -114,13 +114,13 @@ void Tree:: insert(String value, Node *leaf)
             newNode = new Node(); //need this?
             leaf -> left=newNode; //points to left of current node 
             newNode -> key_value = value; //sets equal to sequence value
-            newNode -> left = NULL; //sets new node's left child to null
-            newNode -> right = NULL; //sets new node's right child to null
+            newNode -> left = '/n'; //sets new node's left child to null
+            newNode -> right = '/n'; //sets new node's right child to null
         }
     } 
     else if(value >= leaf -> key_value)
     {
-        if(leaf -> right!= NULL)
+        if(leaf -> right!= '/n') //fix to null
         {
             insert(value, leaf -> right); //recursive call
         }
@@ -130,8 +130,8 @@ void Tree:: insert(String value, Node *leaf)
             newNode = new Node();
             leaf -> right = newNode; //points to right of current node
             newNode -> key_value = value; //sets equal to sequence value
-            newNode -> left = NULL; //sets new node's left child to null
-            newNode -> right = NULL; //sets new node's right child to null
+            newNode -> left = '/n'; //sets new node's left child to null
+            newNode -> right = '/n'; //sets new node's right child to null
         }
     }
 }
@@ -143,7 +143,7 @@ void print(Node *root) //in order traverse print
     //then print root's right child, and that node's right child, etc
     //until reach the right most node
     
-    if(root != NULL)
+    if(root != '/n') //fix to null
     {
         print(root -> left); 
         //need to print root
